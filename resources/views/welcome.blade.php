badeaa4da1e11d0f2cb79e670c2a0b014ad6f4a6
Dang Thang <thang.pham@fujitechjsc.com>
2021-04-08 14:04:19 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,7 +46,7 @@
                 </div>
 
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                    <h1>Hello word (Slack Notification)</h1>
+                    <h1>Hello word (Slack Notification 1)</h1>
                 </div>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
